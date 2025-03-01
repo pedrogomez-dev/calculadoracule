25cc394fe4dce994f4f92c533ef58035d189018a
pedrogomez-dev <pipefinn@gmail.com>
2025-03-01 15:51:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,22 +5,12 @@ function tocar(value) {
     document.querySelector('.pantalla').value += value;
     console.log(value);
 }
-async function resultado(){
-    const display = document.querySelector('.pantalla');
-    a = true;
-    while (a == true){
-        a = false;
-        document.querySelector('.pantalla').value = 6;
-        await esperar(3000);
-        document.querySelector('.pantalla').value = 'Ese no es el resultado real.';
-        await esperar(1000);
-    }
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
-    }
+function resultado(){
+    document.querySelector('.pantalla').value = 6;
 }
 function clearDisplay() {
     document.querySelector('.pantalla').value = '';
 }
+
+let sonido = new Audio();
+sonido.src = './sonido.mp3';
